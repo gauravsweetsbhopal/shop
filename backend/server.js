89462bf94dd8ced715e8e969876ee5e0bfdee484
gauravsweetsbhopal <vivekkirtania484440@gmail.com>
2026-05-11 05:26:43 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const fs = require("fs");
+const path = require('path');
 const nodemailer = require("nodemailer");
 
 const app = express();
